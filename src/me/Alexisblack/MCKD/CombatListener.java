aa97ae00f674c1a7d423aa84aad1afdafa8a0292
Omar S <osanchez@umass.edu>
2017-11-25 01:42:15 -0500

    kills and death tracking functionality finished. Needs testing

--- src/me/Alexisblack/MCKD/CombatListener.java
+++ src/me/Alexisblack/MCKD/CombatListener.java
@@ -1,8 +1,12 @@
 package me.Alexisblack.MCKD;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.event.EventHandler;
+
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -11,45 +15,94 @@ public class CombatListener implements Listener {
 
     public static HashMap<UUID, KDA> combatLogs = new HashMap<>();
 
-    //adds a new player to the hash map
-    public void addPlayerLog(UUID player, KDA newKDA) {
-        combatLogs.put(player, newKDA);
+    //display any important information to the player
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        //Alpha Testing Message
+        Bukkit.broadcastMessage("Welcome " + event.getPlayer().getName() + " to KitPVP! We are trying a new K/D/A system. Please report any " +
+                "bugs you encounter to server admin.");
+
+        //add score board to player
+        Player player = event.getPlayer();
+        player.setScoreboard(Main.statsBoard);
+
+        try {
+            //load players kda from hash map
+            KDA playerKDA = combatLogs.get(player.getUniqueId());
+            String playerKills = Integer.toString(playerKDA.getKills());
+            String playerDeaths = Integer.toString(playerKDA.getDeaths());
+            String playerAssists = Integer.toString(playerKDA.getAssists());
+
+            //update scoreboard with hash map values
+            Scoreboard board = player.getScoreboard();
+            board.getTeam("Kills").setSuffix(playerKills);
+            board.getTeam("Deaths").setSuffix(playerDeaths);
+            board.getTeam("Assists").setSuffix(playerAssists);
+        } catch (Exception e) { //key not found exception, create new entry for hash
+            //create new KDA object and add it to the hash map with key player uuid
+            KDA newKDA = new KDA();
+            UUID newPlayer = event.getPlayer().getUniqueId();
+            combatLogs.put(newPlayer, newKDA);
+
+            //add 0's to scoreboard as place values since the player was not in the hash map
+            Scoreboard board = player.getScoreboard();
+            board.getTeam("Kills").setSuffix(Integer.toString(0));
+            board.getTeam("Deaths").setSuffix(Integer.toString(0));
+            board.getTeam("Assists").setSuffix(Integer.toString(0));
+
+            //TODO: add default values of 0 to the scoreboard so that its not blank before a player kills player
+        }
     }
 
     //Handles kills and deaths of players
     @EventHandler
         public void onPlayerKill(PlayerDeathEvent event) {
-        if(event.getEntity() instanceof Player) {
-            final Player defender = (Player) event.getEntity();
-            UUID defenderID = defender.getUniqueId();
-            if(event.getEntity().getKiller() instanceof Player) {
-                final Player attacker = (Player) event.getEntity().getKiller();
-                UUID attackerID = attacker.getUniqueId();
-                if(combatLogs.containsKey(attackerID)) {
-                    //increment killers kills
-                    KDA attackerKDA = combatLogs.get(attackerID);
-                    attackerKDA.incrementKills();
-                    combatLogs.replace(attackerID, attackerKDA);
-                } else {
-                    //add killer to hash map for the first time
-                    KDA newKiller = new KDA();
-                    newKiller.incrementKills();
-                    combatLogs.put(attackerID, newKiller);
-                }
-                if(combatLogs.containsKey(defenderID)) {
-                    //increment defenders deaths
-                    KDA defenderKDA = combatLogs.get(defenderID);
-                    defenderKDA.incrementDeaths();
-                    combatLogs.replace(defenderID, defenderKDA);
-                } else {
-                    //add defender to hash map for the first time
-                    KDA newDefender = new KDA();
-                    newDefender.incrementKills();
-                    combatLogs.put(defenderID, newDefender);
-                }
+            if (event.getEntity() instanceof Player) {
+                final Player defender = event.getEntity();
+                UUID defenderID = defender.getUniqueId();
+                if (event.getEntity().getKiller() instanceof Player) {
+                    final Player attacker = event.getEntity().getKiller();
+                    UUID attackerID = attacker.getUniqueId();
+                    //attacker
+                    if (combatLogs.containsKey(attackerID)) {
+                        //increment kills in hash map
+                        KDA attackerKDA = combatLogs.get(attackerID);
+                        attackerKDA.incrementKills();
+                        combatLogs.replace(attackerID, attackerKDA);
+                        //update scoreboard
+                        Scoreboard attackerBoard = attacker.getScoreboard();
+                        attackerBoard.getTeam("Kills").setSuffix(Integer.toString(attackerKDA.getKills()));
 
+                    } else {
+                        //add killer to hash map for the first time
+                        KDA newKiller = new KDA();
+                        newKiller.incrementKills();
+                        combatLogs.put(attackerID, newKiller);
+                        //update scoreboard
+                        Scoreboard attackerBoard = attacker.getScoreboard();
+                        attackerBoard.getTeam("Kills").setSuffix(Integer.toString(newKiller.getKills()));
+                    }
+                    //defender
+                    if (combatLogs.containsKey(defenderID)) {
+                        //increment deaths in hashmap
+                        KDA defenderKDA = combatLogs.get(defenderID);
+                        defenderKDA.incrementDeaths();
+                        combatLogs.replace(defenderID, defenderKDA);
+                        //update scoreboard
+                        Scoreboard defenderBoard = defender.getScoreboard();
+                        defenderBoard.getTeam("Deaths").setSuffix(Integer.toString(defenderKDA.getDeaths()));
+                    } else {
+                        //add defender to hash map for the first time
+                        KDA newDefender = new KDA();
+                        newDefender.incrementKills();
+                        combatLogs.put(defenderID, newDefender);
+                        //update scoreboard
+                        Scoreboard defenderBoard = defender.getScoreboard();
+                        defenderBoard.getTeam("Deaths").setSuffix(Integer.toString(newDefender.getDeaths()));
+                    }
+
+                }
             }
-        }
     }
 
     //TODO: Listener for Assists
--- src/me/Alexisblack/MCKD/Main.java
+++ src/me/Alexisblack/MCKD/Main.java
@@ -1,20 +1,13 @@
 package me.Alexisblack.MCKD;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.scoreboard.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.UUID;
-
-
 public class Main extends JavaPlugin implements Listener {
-    private Scoreboard statsBoard;
-    private Objective stats;
-
+    public static Scoreboard statsBoard;
+    public static Objective stats;
 
     @Override
     public void onEnable() {
@@ -33,15 +26,12 @@ public class Main extends JavaPlugin implements Listener {
         //kills, Deaths, Assists team deceleration and initialization
         Team kills = statsBoard.registerNewTeam("Kills");
         kills.addEntry(ChatColor.GREEN + "Kills: ");
-        kills.setPrefix("0");
 
         Team deaths = statsBoard.registerNewTeam("Deaths");
         deaths.addEntry(ChatColor.RED + "Deaths: ");
-        deaths.setPrefix("0");
 
         Team assists = statsBoard.registerNewTeam("Assists");
         assists.addEntry(ChatColor.YELLOW + "Assists: ");
-        assists.setPrefix("0");
 
         //set scoreboard positions
         stats.getScore(ChatColor.GREEN + "Kills: ").setScore(0);
@@ -52,39 +42,5 @@ public class Main extends JavaPlugin implements Listener {
     @Override
     public void onDisable() {
         //runs then the server stops and disables all plugins
-    }
-
-
-    //display any important information to the player
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        //Alpha Testing Message
-        Bukkit.broadcastMessage("Welcome " + event.getPlayer().getName() + " to KitPVP! We are trying a new K/D/A system. Please report any " +
-                "bugs you encounter to server admin.");
-
-        //add score board to player
-        Player player = event.getPlayer();
-        player.setScoreboard(statsBoard);
-
-        try {
-            //load players kda from hash map
-            KDA playerKDA = CombatListener.combatLogs.get(player.getUniqueId());
-            String playerKills = Integer.toString(playerKDA.getKills());
-            String playerDeaths = Integer.toString(playerKDA.getDeaths());
-            String playerAssists = Integer.toString(playerKDA.getAssists());
-
-            //update scoreboard with hash map values
-            Scoreboard board = player.getScoreboard();
-            board.getTeam("Kills").setPrefix(playerKills);
-            board.getTeam("Deaths").setPrefix(playerDeaths);
-            board.getTeam("Assists").setPrefix(playerAssists);
-        } catch (Exception e) { //key not found exception, create new entry for hash
-            //create new KDA object and add it to the hash map with key player uuid
-            KDA newKDA = new KDA();
-            UUID newPlayer = event.getPlayer().getUniqueId();
-            CombatListener.combatLogs.put(newPlayer, newKDA);
-        }
-
-
     }
 }
